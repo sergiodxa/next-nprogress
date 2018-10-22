1ead2b03dbb1aee1c2e157f70c7bf91b0a42dea2
Sergio Xalambrí <sergio@able.co>
2018-10-22 10:12:46 -0500

    Pass linter, move styles to component implementation

--- src/component.js
+++ src/component.js
@@ -1,7 +1,6 @@
 import React from "react";
 import NProgress from "nprogress";
 import { withRouter } from "next/router";
-import NProgressStyles from "./styles";
 
 class NProgressContainer extends React.Component {
   static defaultProps = {
@@ -12,16 +11,16 @@ class NProgressContainer extends React.Component {
 
   timer = null;
 
-  componentDidMount () {
+  componentDidMount() {
     const { options, router, showAfterMs } = this.props;
 
     if (options) {
       NProgress.configure(options);
     }
 
-    const previousChangeStartCallback = Router.onRouteChangeStart
-    const previousChangeCompleteCallback = Router.onRouteChangeComplete
-    const previousChangeErrorCallback = Router.onRouteChangeError
+    const previousChangeStartCallback = Router.onRouteChangeStart;
+    const previousChangeCompleteCallback = Router.onRouteChangeComplete;
+    const previousChangeErrorCallback = Router.onRouteChangeError;
     router.onRouteChangeStart = () => {
       if (typeof previousChangeStartCallback === "function") {
         previousChangeStartCallback();
@@ -47,14 +46,94 @@ class NProgressContainer extends React.Component {
     };
   }
 
-  componentWillUnmount () {
+  componentWillUnmount() {
     clearTimeout(this.timer);
   }
 
-  render () {
+  render() {
     const { color, spinner } = this.props;
+
     return (
-      <NProgressStyles color={color} spinner={spinner} />
+      <style jsx global>{`
+        #nprogress {
+          pointer-events: none;
+        }
+
+        #nprogress .bar {
+          background: ${color};
+
+          position: fixed;
+          z-index: 1031;
+          top: 0;
+          left: 0;
+
+          width: 100%;
+          height: 2px;
+        }
+
+        #nprogress .peg {
+          display: block;
+          position: absolute;
+          right: 0px;
+          width: 100px;
+          height: 100%;
+          box-shadow: 0 0 10px ${color}, 0 0 5px ${color};
+          opacity: 1;
+
+          -webkit-transform: rotate(3deg) translate(0px, -4px);
+          -ms-transform: rotate(3deg) translate(0px, -4px);
+          transform: rotate(3deg) translate(0px, -4px);
+        }
+
+        #nprogress .spinner {
+          display: ${spinner ? "block" : "none"};
+          position: fixed;
+          z-index: 1031;
+          top: 15px;
+          right: 15px;
+        }
+
+        #nprogress .spinner-icon {
+          width: 18px;
+          height: 18px;
+          box-sizing: border-box;
+
+          border: solid 2px transparent;
+          border-top-color: ${color};
+          border-left-color: ${color};
+          border-radius: 50%;
+
+          -webkit-animation: nprogresss-spinner 400ms linear infinite;
+          animation: nprogress-spinner 400ms linear infinite;
+        }
+
+        .nprogress-custom-parent {
+          overflow: hidden;
+          position: relative;
+        }
+
+        .nprogress-custom-parent #nprogress .spinner,
+        .nprogress-custom-parent #nprogress .bar {
+          position: absolute;
+        }
+
+        @-webkit-keyframes nprogress-spinner {
+          0% {
+            -webkit-transform: rotate(0deg);
+          }
+          100% {
+            -webkit-transform: rotate(360deg);
+          }
+        }
+        @keyframes nprogress-spinner {
+          0% {
+            transform: rotate(0deg);
+          }
+          100% {
+            transform: rotate(360deg);
+          }
+        }
+      `}</style>
     );
   }
 }
--- src/hoc.js
+++ src/hoc.js
@@ -1,14 +1,15 @@
 import { Component } from "react";
 import NProgress from "./component";
 
-export default (delayMs, options) => Page => class extends Component {
-  static getInitialProps = Page.getInitialProps;
-  render() {
-    return (
-      <>
-        <Page {...props} />
-        <NProgress delayMs={delayMs} options={options} />
-      </>
-    )
-  }
-}
+export default (delayMs, options) => Page =>
+  class extends Component {
+    static getInitialProps = Page.getInitialProps;
+    render() {
+      return (
+        <>
+          <Page {...props} />
+          <NProgress delayMs={delayMs} options={options} />
+        </>
+      );
+    }
+  };
