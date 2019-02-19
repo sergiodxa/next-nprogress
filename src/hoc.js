3a00d84015ca55ff3cfb9d2d77f628bbfe062b6e
Decebal Dobrica (Deci) <decebal1988@gmail.com>
2019-02-19 12:28:24 +0000

    fix: 🐛 Nprogress misconfigured by default, add options default
    Issues: #83

--- src/hoc.js
+++ src/hoc.js
@@ -1,14 +1,19 @@
 import React, { Component } from "react";
 import NProgress from "./component";
 
-export default (delayMs, options) => Page =>
+export default (delayMs, options = {}) => Page =>
   class extends Component {
     static getInitialProps = Page.getInitialProps;
     render() {
       return (
         <>
           <Page {...this.props} />
-          <NProgress delayMs={delayMs} options={options} />
+          <NProgress
+            options={options}
+            showAfterMs={delayMs}
+            color={options.color}
+            spinner={options.showSpinner}
+          />
         </>
       );
     }
